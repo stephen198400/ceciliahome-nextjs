0ab9bacbb148291384811b225a80cc1b6480bd23
Cicada <89372746+stephen198400@users.noreply.github.com>
2025-03-20 10:23:50 +0800

    feat: integrate Microsoft Clarity script into layout for enhanced user behavior tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from '@/components/common/footer';
 import { Nav } from '@/components/common/nav';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -48,6 +49,17 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<Script id="microsoft-clarity" strategy="afterInteractive">
+					{`
+					(function(c,l,a,r,i,t,y){
+						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window, document, "clarity", "script", "qqyd2btfyu");
+					`}
+				</Script>
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased `}
 			>
